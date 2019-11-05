ad4db63dda94d95d8b54a6e13769e52a164e9ee2
ndirangu <ndirangu.muchai@gmail.com>
2019-11-05 19:33:20 +0300

    initial commit

---
+++ src/main/java/com/ndirangu/scoutwatch/ScoutWatchApplication.java
@@ -0,0 +1,13 @@
+package com.ndirangu.scoutwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoutWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoutWatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ndirangu/scoutwatch/ScoutWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndirangu.scoutwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoutWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
