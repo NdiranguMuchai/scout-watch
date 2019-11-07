10a8c603f449287b682032dece9fa22e601df79c
ndirangu <ndirangu.muchai@gmail.com>
2019-11-07 19:01:37 +0300

    swagger annotations added to coach controller

--- src/main/java/com/ndirangu/scoutwatch/controller/CoachController.java
+++ src/main/java/com/ndirangu/scoutwatch/controller/CoachController.java
@@ -2,6 +2,8 @@ package com.ndirangu.scoutwatch.controller;
 
 import com.ndirangu.scoutwatch.model.Coach;
 import com.ndirangu.scoutwatch.service.CoachService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(value = "Coach",tags = {"Coach"})
 @RequestMapping("/coach")
 public class CoachController {
     private CoachService coachService;
@@ -17,6 +20,7 @@ public class CoachController {
         this.coachService = coachService;
     }
 
+    @ApiOperation(value = "view a list of all coaches ", notes = "views a list of all coaches")
     @GetMapping
     public Page<Coach> listAll(Pageable pageable){
         return coachService.listAll(pageable);
