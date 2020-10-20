94b48abc9a35d8530db9058650fe4fb4d334aba9
ndirangu <ndirangu.muchai@gmail.com>
2020-10-21 00:59:39 +0300

    added index controller and template

---
+++ src/main/java/com/ndirangu/scoutwatch/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.ndirangu.scoutwatch.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping({"","/","/index","/index.html"})
+@RestController
+public class IndexController {
+    public String index(){
+        return "index";
+    }
+}
