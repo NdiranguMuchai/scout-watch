847eed4d08c5c61c30ac0dda9bec5fb28b33c826
ndirangu <ndirangu.muchai@gmail.com>
2020-10-26 13:00:53 +0300

    added navbar to index template

--- src/main/java/com/ndirangu/scoutwatch/controller/IndexController.java
+++ src/main/java/com/ndirangu/scoutwatch/controller/IndexController.java
@@ -1,6 +1,5 @@
 package com.ndirangu.scoutwatch.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/com/ndirangu/scoutwatch/dataloader/Dataloader.java
+++ src/main/java/com/ndirangu/scoutwatch/dataloader/Dataloader.java
@@ -4,31 +4,29 @@ import com.ndirangu.scoutwatch.model.Club;
 import com.ndirangu.scoutwatch.model.Coach;
 import com.ndirangu.scoutwatch.model.Player;
 import com.ndirangu.scoutwatch.model.Position;
-import com.ndirangu.scoutwatch.service.ClubService;
-import com.ndirangu.scoutwatch.service.CoachService;
-import com.ndirangu.scoutwatch.service.PlayerService;
-import com.ndirangu.scoutwatch.service.PositionService;
+import com.ndirangu.scoutwatch.repository.ClubRepository;
+import com.ndirangu.scoutwatch.repository.CoachRepository;
+import com.ndirangu.scoutwatch.repository.PlayerRepository;
+import com.ndirangu.scoutwatch.repository.PositionRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.UUID;
 
 @Component
 public class Dataloader implements CommandLineRunner {
-    private final ClubService clubService;
-    private final CoachService coachService;
-    private final PlayerService playerService;
-    private final PositionService positionService;
+    private final ClubRepository clubRepository;
+    private final CoachRepository coachRepository;
+    private final PlayerRepository playerRepository;
+    private final PositionRepository positionRepository;
 
-
-    public Dataloader(ClubService clubService,
-                      CoachService coachService,
-                      PlayerService playerService,
-                      PositionService positionService) {
-        this.clubService = clubService;
-        this.coachService = coachService;
-        this.playerService = playerService;
-        this.positionService = positionService;
+    public Dataloader(ClubRepository clubRepository,
+                      CoachRepository coachRepository,
+                      PlayerRepository playerRepository,
+                      PositionRepository positionRepository) {
+        this.clubRepository = clubRepository;
+        this.coachRepository = coachRepository;
+        this.playerRepository = playerRepository;
+        this.positionRepository = positionRepository;
     }
 
     @Override
@@ -37,33 +35,33 @@ public class Dataloader implements CommandLineRunner {
         Position gola = new Position();
         gola.setName("right back");
         gola.setAbbreviation("RB");
-        UUID savedGolaId = positionService.create(gola);
+        Position savedGola = positionRepository.save(gola);
 
         //position2
         Position centerBack = new Position();
         centerBack.setName("centre back");
         centerBack.setAbbreviation("CB");
-         positionService.create(centerBack);
+         positionRepository.save(centerBack);
 
 
         //coach
         Coach sting = new Coach();
         sting.setName("Coach Sido");
-        UUID savedStingId = coachService.create(sting);
+        Coach savedSting = coachRepository.save(sting);
 
         //coach2
         Coach atieri = new Coach();
         atieri.setName("Coach Atieri");
-         coachService.create(atieri);
+         coachRepository.save(atieri);
 
 
         //club
         Club waleWasee = new Club();
         waleWasee.setName("Rurie");
 
-        waleWasee.setCoach(coachService.findOne(savedStingId).orElseThrow(()-> new Exception("Sting not found")));
+        waleWasee.setCoach(savedSting);
 
-        UUID savedWaleeWaseeId = clubService.create(waleWasee);
+       Club savedWaleeWasee = clubRepository.save(waleWasee);
 
         //club 2
         Club kiamumbi = new Club();
@@ -71,15 +69,15 @@ public class Dataloader implements CommandLineRunner {
 
         kiamumbi.setCoach(atieri);
 
-         clubService.create(kiamumbi);
+         clubRepository.save(kiamumbi);
 
         //player1
         Player dira = new Player();
         dira.setName("drogba");
 
-        dira.getPositions().add(positionService.findOne(savedGolaId).orElseThrow(()-> new Exception("Gola not found")));
-        dira.setClub(clubService.findOne(savedWaleeWaseeId).orElseThrow(()-> new Exception("Wale wasee not found")));
-        playerService.create(dira);
+        dira.getPositions().add(savedGola);
+        dira.setClub(savedWaleeWasee);
+        playerRepository.save(dira);
 
         //player 2
         Player asena = new Player();
@@ -87,7 +85,7 @@ public class Dataloader implements CommandLineRunner {
 
         asena.getPositions().add(centerBack);
         asena.setClub(kiamumbi);
-        playerService.create(asena);
+        playerRepository.save(asena);
 
     }
 }
